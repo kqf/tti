1d5eaa64361f2c43e332b180ae8ff8af795497d4
Sasha <q.dev.null@gmail.com>
2022-05-23 08:24:20 +0100

    Fix the console scripts

--- setup.py
+++ setup.py
@@ -4,4 +4,9 @@ setup(
     name="portray",
     version="0.0.1",
     packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "tti=potray.__main__:main",
+        ],
+    },
 )
