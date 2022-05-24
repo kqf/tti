53248cacfd76f0363a4bf0f7316e24d1ef7daf9e
Sasha <q.dev.null@gmail.com>
2022-05-24 13:49:06 +0100

    Refactor the plotting function

--- portray/__main__.py
+++ portray/__main__.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot(text, canvas):
+def plot(text, canvas, ofile):
     plt.text(
         .5,
         .5,
@@ -18,18 +18,14 @@ def plot(text, canvas):
     plt.gca().axis('off')
     plt.gcf().tight_layout()
     plt.imshow(canvas)
-    plt.savefig(
-        f"{text.replace(' ', '-')}.png",
-        bbox_inches="tight",
-        pad_inches=0
-    )
+    plt.savefig( ofile, bbox_inches="tight", pad_inches=0)
     plt.show()
 
 
 def main():
     canvas = np.zeros((1024, 1024, 3), dtype=np.uint8)
     canvas[:, :, :] = 238, 235, 217
-    plot("Sample text", canvas)
+    plot("Sample text", canvas, "test.png")
 
 
 if __name__ == "__main__":
